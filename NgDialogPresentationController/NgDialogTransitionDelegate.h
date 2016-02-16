b77a7e3aaa2f4f9ef24f06b54b0bb8854da67be2
Meiwin <meiwin@pie.co>
2016-02-16 15:09:14 +0800

    update copyrights.

--- NgDialogPresentationController/NgDialogTransitionDelegate.h
+++ NgDialogPresentationController/NgDialogTransitionDelegate.h
@@ -3,7 +3,7 @@
 //  Search
 //
 //  Created by Meiwin Fu on 20/9/15.
-//  Copyright © 2015 BlockThirty. All rights reserved.
+//  Copyright © 2015 Meiwin Fu. All rights reserved.
 //
 
 @import UIKit;
